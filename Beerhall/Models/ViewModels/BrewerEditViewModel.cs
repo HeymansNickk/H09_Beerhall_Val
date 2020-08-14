94109bd0d9096e64e486f87ca29c853c37d3621a
Nick Heymans <nick.heymans@hotmail.com>
2020-08-14 10:02:40 +0200

    Add project files.

---
+++ Beerhall/Models/ViewModels/BrewerEditViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Beerhall.Models.Domain;
+using System;
+
+namespace Beerhall.Models.ViewModels {
+    public class BrewerEditViewModel {
+        public string Name { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public int? Turnover { get; set; }
+        public string Description { get; set; }
+        public string ContactEmail { get; set; }
+        public DateTime? DateEstablished { get; set; }
+
+        public BrewerEditViewModel() {
+        }
+
+        public BrewerEditViewModel(Brewer brewer) {
+            Name = brewer.Name;
+            Street = brewer.Street;
+            PostalCode = brewer.Location?.PostalCode;
+            ContactEmail = brewer.ContactEmail;
+            Description = brewer.Description;
+            DateEstablished = brewer.DateEstablished;
+            Turnover = brewer.Turnover;
+        }
+    }
+}
