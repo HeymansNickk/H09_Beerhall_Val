ad6e02b54246008be7f0411f02f5bdf848e9b3c0
Nick Heymans <nick.heymans@hotmail.com>
2020-08-14 10:23:26 +0200

    Add Display and DataType annotations

--- Beerhall/Models/ViewModels/BrewerEditViewModel.cs
+++ Beerhall/Models/ViewModels/BrewerEditViewModel.cs
@@ -1,20 +1,31 @@
 ﻿using Beerhall.Models.Domain;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Beerhall.Models.ViewModels {
     public class BrewerEditViewModel {
         public string Name { get; set; }
+        [Display(Name = "Street", Prompt = "Street and house number")]
         public string Street { get; set; }
         public string PostalCode { get; set; }
+
+        [DataType(DataType.Currency)]
         public int? Turnover { get; set; }
         public string Description { get; set; }
+
+        [Display(Name = "Email address")]
+        [DataType(DataType.EmailAddress)]
         public string ContactEmail { get; set; }
+
+        [Display(Name = "Date established")]
+        [DataType(DataType.Date)]
         public DateTime? DateEstablished { get; set; }
 
         public BrewerEditViewModel() {
         }
 
-        public BrewerEditViewModel(Brewer brewer) {
+        public BrewerEditViewModel(Brewer brewer)
+        {
             Name = brewer.Name;
             Street = brewer.Street;
             PostalCode = brewer.Location?.PostalCode;
